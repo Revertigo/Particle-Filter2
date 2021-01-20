6f126c04cfb3a4dbd141d11008b90ccade79bc76
Revertigo <dekel241@gmail.com>
2021-01-21 01:38:48 +0200

    Add legend to visualization

--- visualization.py
+++ visualization.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-delay = 0.01
+delay = 0.000001
+
 
 def displaytrajectory(particles, bestP, gt, plot_all=True, disp=True):
     prts = np.array([[p.x, p.y, p.weight] for p in particles])
     prts[:, 2] /= prts[:, 2].sum()
 
     if plot_all:
-        plt.scatter(prts[:, 0], prts[:, 1], c=prts[:, 2], cmap='gray', s=10)
+        plt.scatter(prts[:, 0], prts[:, 1], c=prts[:, 2], cmap='gray', s=10, edgecolors='grey')
+
     plt.plot(gt[0], gt[1], 'g*')
     plt.plot(bestP.x, bestP.y, 'c*')
+    # locate the legend at top left corner
+    plt.legend((['GT', 'Estimation']), loc=2)
 
     if disp:
         plt.pause(delay)
